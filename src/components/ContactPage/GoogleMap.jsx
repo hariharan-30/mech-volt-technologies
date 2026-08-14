8d9bf68fbc7c889718ac82ca840eef017e76a339
hariharan-30 <hariharan300901@gmail.com>
2026-08-14 10:36:45 +0530

    Google map change

--- src/components/ContactPage/GoogleMap.jsx
+++ src/components/ContactPage/GoogleMap.jsx
@@ -27,11 +27,11 @@ const GoogleMap = () => {
           ></iframe> */}
           <iframe
             title="Google Map"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3943.990794174079!2d77.73580577506199!3d8.692422991356116!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b040ddb22236947%3A0xf651948893b827fc!2seFloz%20IT%20Services!5e0!3m2!1sen!2sin!4v1784968632700!5m2!1sen!2sin"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d635.3011028671974!2d77.56148554924785!3d8.467657488093193!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b04680278a2fa99%3A0x93cbc859e8b04da0!2z4K6J4K6y4K6V4K6z4K6o4K-N4K6kIOCuqOCuvuCupOCuqeCvjSDgrqrgrqTgrr8sIOCuieCun-CviOCur-Cun-Cuv-CupOCun-CvjeCun-CvgQ!5e0!3m2!1sen!2sin!4v1786419941161!5m2!1sen!2sin"
             width="100%"
             height="500"
-            loading="lazy"
             allowFullScreen
+            loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
